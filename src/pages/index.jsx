a14a395d30b5c1c46857bb7c231fb11f174eee39
Mohammad Waqar <waqarqadri6@gmail.com>
2024-06-30 03:40:18 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,15 @@
 import { useRouter } from "next/router"
-import { Fragment } from "react"
-
-import LandingPage from "@/layouts/LandingPage"
-import Banner from "@/components/web/Banner"
-import HRSolution from "@/components/web/HRSolution"
-import HRStrategy from "@/components/web/HRStrategy"
-import HRSystems from "@/components/web/HRSystems"
-import JobManagement from "@/components/web/JobManagement"
-import APIEnabled from "@/components/web/APIEnabled"
-import HRProcess from "@/components/web/HRProcess"
+import { Fragment, useEffect } from "react"
+import Auth from "@/layouts/Auth"
+import ls from 'localstorage-slim';
 
 export default function Home() {
     const router = useRouter()
-    const pageName = router.pathname.substring(1);
-
-    return (
-        <Fragment>
-            <Banner />
-            <main id="main" className={`main page landingPageLayout grow flex flex-col py-12 md:py-20 page-${pageName}`}>
-                <HRSolution />
-                <HRStrategy />
-                <HRSystems />
-                <JobManagement />
-                <APIEnabled />
-                <HRProcess />
-            </main>
-        </Fragment>
-    );
+    useEffect(() => {
+        if (!ls.get('auth_user')) {
+            router.push('/sign-in')
+        }
+    }, [router])
+    return (<>Loading</>);
 }
-
-Home.layout = LandingPage;
+Home.layout = Auth;
