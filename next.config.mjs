1b8cdfb38d3cf30c836f24410ad6abb96a3236b2
waqar <waqar.renesis@gmail.com>
2024-07-04 12:16:57 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-import i18next from "./next-i18next.config.js"
 const nextConfig = {
-    i18n: i18next.i18n,
     reactStrictMode: true,
     swcMinify: true,
     images: {
