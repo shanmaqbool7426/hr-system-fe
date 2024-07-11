f9d076862138231b4962230604edbbbe5ec9f58c
Mohammad Waqar <waqarqadri6@gmail.com>
2024-07-11 10:44:58 -0700

    Update AppsState.jsx

--- src/modules/remoteWork/myRemoteWork/AppsState.jsx
+++ src/modules/remoteWork/myRemoteWork/AppsState.jsx
@@ -1,4 +1,4 @@
-import ChangeAppStatusForm from '@/components/forms/remoteWork/ChangeAppStatus'
+import ChangeAppStatusForm from '@/components/forms/remoteWork/changeAppStatus'
 import { ThreeDotsVertical } from '@/components/svg' 
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
