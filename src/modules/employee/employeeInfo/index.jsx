01bd4e40fb6594c652a26a06b8e89df08cbff0eb
nadia <nadia@zaffretech.co>
2024-07-24 12:33:37 +0500

    working

--- src/modules/employee/employeeInfo/index.jsx
+++ src/modules/employee/employeeInfo/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import PersonalInfoCard from './PersonalInfoCard';
 import CompanyInfoCard from './CompanyInfoCard'
@@ -14,30 +14,6 @@ import AppointmentCard from '../documents/AppointmentCard';
 import SeperationCard from './SeperationInformation';
 
 export default function EmployeeInfoModule() {
-  const companyInfo = {
-    "Grade": "A",
-    "Country": "USA",
-    "Station": "Head Office",
-    "Allow Attendance Mobile App": "Yes",
-    "Allow Attendance Web Portal": "No"
-  }
-
-  const experienceInformation = [
-    {
-      "companyName": "Digital Devlopment Inc",
-      "location": "USA",
-      "designation": "UI Designer",
-      "startDate": "22-01-2003",
-      "endDate": "22-01-2005"
-    },
-    {
-      "companyName": "Digital Devlopment Inc",
-      "location": "USA",
-      "designation": "UI Designer",
-      "startDate": "22-01-2003",
-      "endDate": "22-01-2005"
-    }
-  ]
 
   return (
     <div className='grid grid-cols-2 gap-6'>
