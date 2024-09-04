6ba8101ec338b8dc4b80f0ef8bdc0f83e4c652d9
Mohammad Waqar <waqar@zaffretech.co>
2024-09-04 21:47:10 +0500

    Update Timepicker.jsx

--- src/components/elements/Timepicker.jsx
+++ src/components/elements/Timepicker.jsx
@@ -1,7 +1,3 @@
-import 'rc-time-picker/assets/index.css';
-// import RCTimePicker from 'rc-time-picker';
-import moment from 'moment';
-
 export default function TimePicker({
     label,
     value,
@@ -12,19 +8,4 @@ export default function TimePicker({
     ...props
 }) {
     return <>Working on it</>
-    // const now = moment().hour(0).minute(0);
-    // return <div className='zt-formGroup'>
-    //     {label && <label className="dark:text-themeGrayscale300" htmlFor={props?.id}>
-    //         {label} {required && <sup className="text-[1.25rem] text-themeDanger">*</sup>}
-    //     </label>}
-
-    //     <RCTimePicker
-    //         showSecond={false}
-    //         defaultValue={now}
-    //         className="zt-themeInput"
-    //         value={moment(value)}
-    //         onChange={onChange}
-    //         format={'HH:mm'}
-    //     />
-    // </div>
 }
