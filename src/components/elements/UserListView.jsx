81b1e47402bbb0bcde9b71ff2f76a67bc5e79f8f
Mohammad Waqar <waqar@zaffretech.co>
2024-09-28 18:34:38 +0500

    Update UserListView.jsx

--- src/components/elements/UserListView.jsx
+++ src/components/elements/UserListView.jsx
@@ -3,7 +3,7 @@ export default function UserListView({ list, limit = 3, imgClass }) {
   return <ul className='flex pl-4'>
     {list?.slice(0, limit).map((item, index) => (
       <li key={index} className='cursor-pointer -ml-4 shrink-0' title={`${item?.firstName} ${item?.lastName}`}>
-        {!item.avatar == '' ?
+        {!item?.avatar == '' ?
           <figure className={`${imgClass ? `${imgClass}` : "w-12 h-12"} overflow-hidden rounded-full m-0`}>
             <Image src={item.avatar} width={48} height={48} alt={item.firstName} /></figure> :
           <span className={`uppercase relative overflow-hidden text-white font-semibold flex justify-center items-center rounded-full ${imgClass ? `${imgClass}` : "w-12 h-12"} m-0 z-${[index]} bg-themeBlue`}>
